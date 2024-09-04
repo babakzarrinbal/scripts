fadc6b0b2aa1e1b68290ce02f6bb61df1cdb5d38
babak zarrinbal <babak.zarrinbal@gmail.com>
2024-09-04 14:58:20 +0200

    chore: Refactor get-build-folders.sh script for improved readability and efficiency

--- workflows/get-build-folders.sh
+++ workflows/get-build-folders.sh
@@ -10,23 +10,19 @@ git_root=$(git rev-parse --show-toplevel)
 
 # Find all directories containing a .dockerbuild file
 build_dirs=$(find "$git_root" -name "Dockerfile" -exec dirname {} \;)
-echo "checking folders for build: "
-for build_dir in $build_dirs; do
-  echo "$build_dir"
-done
-echo "---"
+
 # Get the list of files changed in the current commit
 changed_files=$(git diff-tree --no-commit-id --name-only -r HEAD)
 selected_build_dirs=""
 # Iterate over each build directory
 for build_dir in $build_dirs; do
+    echo "Checking $build_dir_name for build"
     # Get the name of the build directory
     if [ -f "$build_dir/.dockerbuild" ]; then
       build_dir_name=$(basename "$build_dir")
-      echo "Checking $build_dir_name"
+      echo "Found .dockerbuild file, checking for relevant changes to .dockerbuild"
       # Read the patterns from the .dockerbuild file
       patterns=$(grep -v '^#' "$build_dir/.dockerbuild" | grep -v '^$')
-      echo "Patterns: $patterns"
       # Adjust the paths to be relative to the build directory
       relative_files=$(echo "$changed_files" | grep "^$build_dir_name/" | sed "s|^$build_dir_name/||")
 
@@ -47,7 +43,7 @@ for build_dir in $build_dirs; do
           fi
       done
     else
-      echo "No .dockerbuild file in $build_dir"
+      echo "No .dockerbuild file in $build_dir, adding to build list"
       selected_build_dirs+="$build_dir\n"
       echo "---"
     fi
